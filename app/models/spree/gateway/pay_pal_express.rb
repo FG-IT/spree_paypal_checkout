e4fa01a9ceeb48aad4cab890215f247073cc5fd3
reader <plpm.zhao@gmail.com>
2022-06-03 11:08:12 -0400

    delete unused code

--- app/models/spree/gateway/pay_pal_express.rb
+++ app/models/spree/gateway/pay_pal_express.rb
@@ -1,7 +1,7 @@
 module Spree
   class Gateway::PayPalExpress < Gateway
     
-  end
+
 
   def supports?(source)
     true
@@ -26,186 +26,38 @@ module Spree
   end
 
   def authorize(amount, express_checkout, gateway_options = {})
-    sale(amount, express_checkout, "Authorization", gateway_options)
   end
 
   def settle(amount, checkout, _gateway_options) end
 
   def capture(credit_cents, transaction_id, _gateway_options)
 
-    payment_id = _gateway_options[:order_id].split('-')[-1]
-    payment = Spree::Payment.find_by(number: payment_id)
-    transaction_id = payment.source.transaction_id
-    params = {
-        :AuthorizationID => transaction_id,
-        :CompleteType => "Complete",
-        :Amount => {
-            :currencyID => _gateway_options[:currency],
-            :value => credit_cents.to_f / 100}
-    }
-
-    logger.info params
-
-    do_capture = provider.build_do_capture(params)
-    pp_response = provider.do_capture(do_capture)
-
-
-    if pp_response.success?
-      Spree::PaypalExpressCheckout.find_by(transaction_id: transaction_id).update(state: 'completed')
-
-      begin
-        new_transaction_id = pp_response.do_capture_response_details.payment_info.first.transaction_id
-      rescue
-        new_transaction_id = pp_response.do_capture_response_details.payment_info.transaction_id
-      end
-      logger.info new_transaction_id
-
-      Spree::PaypalExpressCheckout.find_by(transaction_id: transaction_id).update(state: 'completed', transaction_id: new_transaction_id)
-
-      Response.new(true, nil, {:id => new_transaction_id})
-    else
-      class << pp_response
-        def to_s
-          errors.map(&:long_message).join(" ")
-        end
-      end
-
-      pp_response
-    end
   end
 
   def purchase(amount, express_checkout, gateway_options = {})
-    sale(amount, express_checkout, "Sale", gateway_options)
+
   end
 
   def credit(credit_cents, transaction_id, _options)
-    payment = _options[:originator].payment
-    refund(transaction_id, payment, credit_cents)
+    
   end
 
   def cancel(response_code, _source, payment)
-    if response_code.nil?
-      source = _source
-    else
-      source = Spree::PaypalExpressCheckout.find_by(token: response_code)
-    end
-
-    if payment.present? and source.can_credit? payment
-      refund(nil, payment, payment.money.amount_in_cents)
-    else
-      void(source.transaction_id, source, nil)
-    end
+  
   end
 
   def void(response_code, _source, gateway_options)
 
-    if _source.present?
-      source = _source
-    else
-      source = Spree::PaypalExpressCheckout.find_by(token: response_code)
-    end
-
-    void_transaction = provider.build_do_void({
-                                                  :AuthorizationID => source.transaction_id
-                                              })
-
-    do_void_response = provider.do_void(void_transaction)
-
-
-    if do_void_response.success?
-      Spree::PaypalExpressCheckout.find_by(transaction_id: source.transaction_id).update(state: 'voided')
-      # This is rather hackish, required for payment/processing handle_response code.
-      Class.new do
-        def success?
-          true;
-        end
-
-        def authorization
-          nil;
-        end
-      end.new
-    else
-      class << do_void_response
-        def to_s
-          errors.map(&:long_message).join(" ")
-        end
-      end
-
-      do_void_response
-    end
   end
 
   def refund(transaction_id, payment, credit_cents)
-    unless transaction_id.present?
-      transaction_id = payment.source.transaction_id
-    end
 
-    refund_type = payment.money.amount_in_cents == credit_cents ? "Full" : "Partial"
-    params = {
-        :TransactionID => transaction_id,
-        :RefundType => refund_type,
-        :Amount => {
-            :currencyID => payment.currency,
-            :value => credit_cents.to_f / 100},
-        :RefundSource => "any"}
-    logger.info params
-
-    refund_transaction = provider.build_refund_transaction(params)
-    refund_transaction_response = provider.refund_transaction(refund_transaction)
-    if refund_transaction_response.success?
-      payment.source.update({
-                                :refunded_at => Time.now,
-                                :refund_transaction_id => refund_transaction_response.RefundTransactionID,
-                                :state => "refunded",
-                                :refund_type => refund_type
-                            })
-    end
-    refund_transaction_response
   end
 
   private
   
   def sale(amount, express_checkout, payment_action, gateway_options = {})
-    pp_details_request = provider.build_get_express_checkout_details({
-                                                                         :Token => express_checkout.token
-                                                                     })
-    pp_details_response = provider.get_express_checkout_details(pp_details_request)
-
-    pp_request = provider.build_do_express_checkout_payment({
-                                                                :DoExpressCheckoutPaymentRequestDetails => {
-                                                                    :PaymentAction => payment_action,
-                                                                    :Token => express_checkout.token,
-                                                                    :PayerID => express_checkout.payer_id,
-                                                                    :PaymentDetails => pp_details_response.get_express_checkout_details_response_details.PaymentDetails
-                                                                }
-                                                            })
-
-    pp_response = provider.do_express_checkout_payment(pp_request)
-
-
-    if pp_response.success?
-      # We need to store the transaction id for the future.
-      # This is mainly so we can use it later on to refund the payment if the user wishes.
-      begin
-        transaction_id = pp_response.do_express_checkout_payment_response_details.payment_info.first.transaction_id
-      rescue
-        transaction_id = pp_response.do_express_checkout_payment_response_details.payment_info.transaction_id
-      end
-
-      express_checkout.update_column(:transaction_id, transaction_id)
-
-      Response.new(true, nil, {:id => transaction_id})
-
-    else
-      class << pp_response
-        def to_s
-          errors.map(&:long_message).join(" ")
-        end
-      end
-
-      Response.new(false, pp_response, nil)
-
-    end
+    
   end
 
 end
