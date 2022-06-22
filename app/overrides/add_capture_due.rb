78ddd777eced97d1317dbecb302cc50d7ee5fcd4
Sky <befruitful12@gmail.com>
2022-06-22 16:32:36 -0400

    change capture due postion

--- app/overrides/add_capture_due.rb
+++ app/overrides/add_capture_due.rb
@@ -1,10 +1,23 @@
+# Deface::Override.new(
+#   virtual_path: "spree/admin/orders/_payments",
+#   name: "add_capture_due",
+#   insert_bottom: 'div.outstanding-balance',
+#   text: <<-EOF
+#     <% if @order.paypal_checkout&.state == 'AUTHORIZED' %>
+#       <%= Spree.t('paypal.canpture_due', due_time: @order.paypal_checkout.order_valid_time.strftime('%a, %d %b %Y %H:%M:%S')).html_safe %>
+#     <% end %>
+#   EOF
+# )
+
 Deface::Override.new(
-  virtual_path: "spree/admin/orders/_payments",
-  name: "add_capture_due",
-  insert_bottom: 'div.outstanding-balance',
+  virtual_path: "spree/admin/payments/_list",
+  name: "add_capture_due_time",
+  replace: 'td[2]',
   text: <<-EOF
-    <% if @order.paypal_checkout&.state == 'AUTHORIZED' %>
-      <%= Spree.t('paypal.canpture_due', due_time: @order.paypal_checkout.order_valid_time.strftime('%a, %d %b %Y %H:%M:%S')).html_safe %>
-    <% end %>
+    <td>
+      <% if @order.paypal_checkout&.state == 'AUTHORIZED' %>
+        <%= Spree.t('paypal.canpture_due', due_time: pretty_time(@order.paypal_checkout.order_valid_time)).html_safe %>
+      <% end %>
+    </td>
   EOF
 )
