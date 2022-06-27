5417f5822135aa49af5ba44ecdd71b4feb440690
Sky <befruitful12@gmail.com>
2022-06-27 16:48:55 -0400

    fix bug

--- app/overrides/add_pay_later_to_product.rb
+++ app/overrides/add_pay_later_to_product.rb
@@ -8,7 +8,7 @@ Deface::Override.new(
         data-pp-message
         data-pp-placement="product"
         data-pp-style-layout="text"
-        data-pp-style-text-size="<%= :::Spree::PaypalCheckout.find_available_on_front_end_paypal_checkout_method.preferred_pay_later_text_size %>"
+        data-pp-style-text-size="<%= ::Spree::PaypalCheckout.find_available_on_front_end_paypal_checkout_method.preferred_pay_later_text_size %>"
         data-pp-style-logo-type="inline"
         data-pp-style-text-color="black"
         data-pp-amount="<%= default_variant.price_in(current_currency).price_including_vat_for(current_price_options) %>"
